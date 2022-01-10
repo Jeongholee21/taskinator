8d69ad4a728f6cbe1edef017b8685f039133cdbe
jeongholee <jeongholee@hotmail.com>
2022-01-09 23:51:16 -0800

    add task-info

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,28 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 //     tasksToDoEl.appendChild(listItemEl);
 // });
 
-var createTaskHandler = function(event) {
+var createTaskHandler = function (event) {
     event.preventDefault();
-    
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item  
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    //Add HTML
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //listItemEl.className = "task-item";
+    //listItemEl.textContent = taskNameInput;
+
+    tasksToDoEl.appendChild(listItemEl);
 };
 
 // buttonEl.addEventListener("click", createTaskHandler);
